5cf7152d5b825055c553475cee1cac0d976876ab
Clemens <30525105+latoxgaming@users.noreply.github.com>
2017-11-28 23:23:09 +0100

    changed username and removed wrong code

--- database.example.php
+++ database.example.php
@@ -1,11 +1,10 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: hthth
+ * User: Clemens
  * Date: 28.11.2017
  * Time: 23:04
  */
 
-<?php
 $pdo = new PDO('host;dbname=testdb', 'user', '1r3jf132rj2i3dn3n');
 ?>
